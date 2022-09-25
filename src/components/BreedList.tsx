b920bea0e4be9af3d0251cae91e2cc18df682b7e
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-09-25 00:21:59 -0600

    instalacion axios

---
+++ src/components/BreedList.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+
+import { IonSearchbar, IonContent } from '@ionic/react';
+import { trashBin } from 'ionicons/icons';
+import internal from "stream";
+
+
+
+/**
+ * Input de busqueda de la pagina de busqueda
+ */
+interface Height{
+  imperial: string,
+  metric: string
+}
+
+interface Weight{
+  imperial: string,
+  metric: string
+}
+
+interface Image {
+  id: string, 
+  width : number,
+  height : number, 
+  url : string
+}
+
+interface Breed{
+  weight : Weight,
+  height : Height,
+  id : number,
+  name: string,
+  bred_for: string, 
+  breed_group : string,
+  life_span : string,
+  temperament : string, 
+  origin : string, 
+  reference_image_id : string,
+  image : Image
+}
+
+interface BreedListProps{
+  breedArray: string[]
+}
+
+
+const SearchFilter: React.FC<BreedListProps> = ({breedArray}) => 
+	<IonContent>
+		{breedArray}
+	</IonContent>
+
+
+export default SearchFilter
