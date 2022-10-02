a772707f2d1fa2b46d70877d54a2198ef52c7b86
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-10-02 12:12:59 -0600

    test - Configuracion Ath0

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import { Auth0Provider } from "@auth0/auth0-react"
 
-
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const container = document.getElementById("root")
+const root = createRoot(container!)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<Auth0Provider
+    domain="dev-6ubin6a9.us.auth0.com"
+    clientId="jbtCT5tAd5lu4FZtkxHmBuib5wALLRp5"
+    redirectUri="io.ionic.starter://dev-6ubin6a9.us.auth0.com/capacitor/io.ionic.starter/callback">
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Auth0Provider>
+)
